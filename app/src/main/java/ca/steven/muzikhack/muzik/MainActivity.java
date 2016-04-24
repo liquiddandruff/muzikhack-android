ec78c1fe99cfc377250f93078ca81d891ab647b6
Steven Huang <stevenhuang1995@gmail.com>
2016-04-24 08:50:14 +0000

    Move spotify receiver to main activity

--- app/src/main/java/ca/steven/muzikhack/muzik/MainActivity.java
+++ app/src/main/java/ca/steven/muzikhack/muzik/MainActivity.java
@@ -1,11 +1,15 @@
 package ca.steven.muzikhack.muzik;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -14,6 +18,7 @@ import com.muzik.accessory.MzAccessory;
 import com.muzik.accessory.MzConnectionState;
 import com.muzik.accessory.callback.IAccelerometerCallback;
 import com.muzik.accessory.callback.IBatteryLevelCallback;
+import com.muzik.accessory.callback.IMotionCallback;
 import com.muzik.accessory.callback.IMzConnectionStateCallback;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,11 +30,47 @@ public class MainActivity extends AppCompatActivity {
     private GoogleApiClient client;
     private MzAccessory mza;
 
+    private IntentFilter spotifyIntentFilter;
+    static final class BroadcastTypes {
+        static final String SPOTIFY_PACKAGE = "com.spotify.music";
+        static final String PLAYBACK_STATE_CHANGED = SPOTIFY_PACKAGE + ".playbackstatechanged";
+        static final String QUEUE_CHANGED = SPOTIFY_PACKAGE + ".queuechanged";
+        static final String METADATA_CHANGED = SPOTIFY_PACKAGE + ".metadatachanged";
+    }
+    boolean haveMetadata = false;
+    String trackId;
+    String artistName;
+    String albumName;
+    String trackName;
+    private BroadcastReceiver spotifyReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+            String action = intent.getAction();
+            if (action.equals(BroadcastTypes.METADATA_CHANGED)) {
+                trackId = intent.getStringExtra("id");
+                artistName = intent.getStringExtra("artist");
+                albumName = intent.getStringExtra("album");
+                trackName = intent.getStringExtra("track");
+                haveMetadata = true;
+                int trackLengthInSec = intent.getIntExtra("length", 0);
+                // Do something with extracted information...
+            }
+        }
+    };
+
+    private static final String REDIRECT_URI = "yourcustomprotocol://callback";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spotifyIntentFilter = new IntentFilter();
+        spotifyIntentFilter.addAction("com.spotify.music.playbackstatechanged");
+        spotifyIntentFilter.addAction("com.spotify.music.metadatachanged");
+        spotifyIntentFilter.addAction("com.spotify.music.queuechanged");
+        
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
@@ -89,6 +130,15 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, String.format("accel X:%f Y:%f Z:%f N:%f FA:%f SA:%f", x, y, z, norm, forwardAngle, sideAngle));
             }
         });
+        mza.registerForMotions(new IMotionCallback() {
+            @Override
+            public void onMotionDetected(int i) {
+                if(haveMetadata) {
+                    // send to spotify
+                    Log.i(TAG, String.format("Liking song %s - %s on spotify", artistName, trackName));
+                }
+            }
+        }, 4); // bobbing = 4
 
     }
 
@@ -130,5 +180,17 @@ public class MainActivity extends AppCompatActivity {
         );
         AppIndex.AppIndexApi.end(client, viewAction);
         client.disconnect();
+    }
+
+    @Override
+    protected void onResume() {
+        registerReceiver(spotifyReceiver, spotifyIntentFilter);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(spotifyReceiver);
+        super.onPause();
     }
 }
--- app/src/main/java/ca/steven/muzikhack/muzik/SpotifyReceiver.java
+++ app/src/main/java/ca/steven/muzikhack/muzik/SpotifyReceiver.java
@@ -6,8 +6,24 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class SpotifyReceiver extends BroadcastReceiver {
+    static final class BroadcastTypes {
+        static final String SPOTIFY_PACKAGE = "com.spotify.music";
+        static final String PLAYBACK_STATE_CHANGED = SPOTIFY_PACKAGE + ".playbackstatechanged";
+        static final String QUEUE_CHANGED = SPOTIFY_PACKAGE + ".queuechanged";
+        static final String METADATA_CHANGED = SPOTIFY_PACKAGE + ".metadatachanged";
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+        String action = intent.getAction();
+        if (action.equals(BroadcastTypes.METADATA_CHANGED)) {
+            String trackId = intent.getStringExtra("id");
+            String artistName = intent.getStringExtra("artist");
+            String albumName = intent.getStringExtra("album");
+            String trackName = intent.getStringExtra("track");
+            int trackLengthInSec = intent.getIntExtra("length", 0);
+            // Do something with extracted information...
+        }
     }
 }
